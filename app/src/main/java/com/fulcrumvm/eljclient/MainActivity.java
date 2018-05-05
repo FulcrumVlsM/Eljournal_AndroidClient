468c48e692a6316be6ff450fbec36e95601f8129
FulcrumVlsM <misha867458@gmail.com>
2018-05-05 22:53:51 +0600

    Добавлено Navigation Drawer

--- app/src/main/java/com/fulcrumvm/eljclient/MainActivity.java
+++ app/src/main/java/com/fulcrumvm/eljclient/MainActivity.java
@@ -2,10 +2,20 @@ package com.fulcrumvm.eljclient;
 
 import android.Manifest;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,48 +29,101 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private Button mybutton;
     private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mybutton = (Button) findViewById(R.id.myButton);
-        mybutton.setOnClickListener(listener);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
-                345);
-        }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
 
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            retrofit = new Retrofit.Builder().baseUrl("http://eljournal.ddns.net")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
 
-            APIService apiService = retrofit.create(APIService.class);
-            apiService.getData("66E25DCB-1EFF-4B5F-86BA-CD686C503EE0").enqueue(new Callback<Result<Faculty>>() {
-                @Override
-                public void onResponse(Call<Result<Faculty>> call, Response<Result<Faculty>> response) {
-                    if(response.isSuccessful())
-                        mybutton.setText(response.body().Data.Name);
-                    else {
-                        mybutton.setText("Ошибка");
-                        Log.d("http_response",String.valueOf(response.code()));
-                    }
-                }
 
-                @Override
-                public void onFailure(Call<Result<Faculty>> call, Throwable t) {
-                    mybutton.setText("Ошибка очень плохая");
-                    Log.e("failure",t.toString());
-                }
-            });
-        }
+    public void onClick(View v){
+        retrofit = new Retrofit.Builder().baseUrl("http://eljournal.ddns.net")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService apiService = retrofit.create(APIService.class);
+        apiService.getData("66E25DCB-1EFF-4B5F-86BA-CD686C503EE0").enqueue(new Callback<Result<Faculty>>() {
+            @Override
+            public void onResponse(Call<Result<Faculty>> call, Response<Result<Faculty>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Result<Faculty>> call, Throwable t) {
+                Log.e("failure",t.toString());
+            }
+        });
+
     };
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 }
